53dcd9765a8dc53e1399a197fe5957ebf87aaee9
CupOfMakiato <nguyenlmse171333@fpt.edu.vn>
2024-10-09 09:43:44 +0700

    merge main

---
+++ BusManagementSystem/Pages/Shared/User/Login.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BusManagementSystem.Pages.Shared.User
+{
+    public class LoginModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
