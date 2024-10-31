6bf46b6974dcbe6056f4c52940314dd1a58be2f7
MOI\84846 <hungnvse150584@fpt.edu.vn>
2024-10-31 11:09:32 +0700

    Fix Dbcontext

---
+++ BusManagementSystem/Pages/Guest/TicketOnlineFree.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BusManagementSystem.Pages.Guest
+{
+    public class TicketOnlineFreeModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
